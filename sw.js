aa73304cd8ab83b731d0fafe3d12a6c097a831a9
joelpeterson <joel@joelpeterson.biz>
2025-11-20 11:58:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+
+    importScripts("https://cdn.npubpro.com/index.js");
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1763651380242" }, { url: "/sw.js", revision: "1763651380243" }, { url: "/manifest.webmanifest", revision: "1763651380244" }] });
+  
