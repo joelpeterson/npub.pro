32796e847f7640049c43f240371bc6dc5f0cac6a
joelpeterson <joel@joelpeterson.biz>
2025-11-20 13:27:31 -0500

    Update 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 
     importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1763662626471" }, { url: "/joelpeterson/npub.pro/tree/main/sw.js", revision: "1763662626472" }, { url: "/joelpeterson/npub.pro/tree/main/manifest.webmanifest", revision: "1763662626473" }] });
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1763663170260" }, { url: "/npub.pro/sw.js", revision: "1763663170261" }, { url: "/npub.pro/manifest.webmanifest", revision: "1763663170262" }] });
   
